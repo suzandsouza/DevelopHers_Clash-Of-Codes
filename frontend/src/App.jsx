4acd7fad2bcacf347a01f90d4699b8c48e961d29
Martina John <martina07j@gmail.com>
2023-03-04 17:17:15 +0530

    Home page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Footer from "./components/Footer"
 import Home from "./views/Home"
 const App = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <Home />
+      <Footer />
    
     </div>
   )
