36550cb8f12e3f69c1ca7a648a27097dfad17b38
Martina John <martina07j@gmail.com>
2023-03-04 14:51:00 +0530

    UI

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,9 +4,10 @@ function Hero() {
    // const[stats]=useGlobalState('stats')
   return (
     <div className='text-center bg-white text-gray-800 py-24 px-6'>
-     <h1 className='text-5xl md:text-6xl font-bold tracking-tight mb-12'> 
+     <h1 className='text-4xl md:text-5xl font-bold tracking-tight mb-12'> 
         <span className='capitalize'>
-        Bringing art to life on 
+        Decentralizing your Banking Experience
+        <br />
         <span className='uppercase text-green-600'> DevHers</span>
             <br />
           
@@ -14,12 +15,12 @@ function Hero() {
         
      </h1>
      <div className='flex justify-center items-center space-x-2'>
-        <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Add Art</button>
+        <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Login</button>
         
         <button type='button' className='"inline-block px-6 py-2.5 border border-green-600
         font-medium text-xs leading-tight uppercase text-green-600
         rounded-full shadow-md bg-transparent hover:bg-green-700
-        hover:text-white'>Back projects</button>
+        hover:text-white'>Signup</button>
      </div>
      <div className='flex justify-center items-center mt-10'>
         <div className='flex flex-col justify-center items-center h-20 border shadow-md w-full'>
